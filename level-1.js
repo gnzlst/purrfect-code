b0c5fec9029460f495cec2244161b93f4f665952
gnzlst <gonzalo.soto@darkom.cl>
2024-08-24 11:55:36 +0930

    Add descriptive comment

--- level-1.js
+++ level-1.js
@@ -1,1 +1,2 @@
+// Array where 0=North, 1=South, 2=East, 3=West
 [0, 0, 1, 1, 1, 1, 3, 0, 0, 3, 2, 2, 2, 2].forEach((b) => window["move" + ["North", "South", "East", "West"][b]]()) + activateTeleporter()
